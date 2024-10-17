a0ec9f97a5a6df61a4377104f1c2fa37c32a9ff0
abdulhadi240 <ah912425@gmail.com>
2024-10-17 17:48:18 +0500

    header

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-black dark:text-white`}
       >
-      
+        <HeaderSection/>
         {children}
         <Footer/>
       </body>
